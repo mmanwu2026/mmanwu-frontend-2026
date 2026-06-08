546306477c065fcf6663aaf643fc77f4d94a17aa
mmanwu2026 <info@taragynecology.com>
2026-06-08 18:56:54 -0400

    rename globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // force rebuild
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals-v2.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
