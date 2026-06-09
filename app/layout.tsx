b6b8b784b97e39ea2f374ca278dca456a8ce3813
mmanwu2026 <info@taragynecology.com>
2026-06-09 00:47:41 -0400

    Change global CSS import to globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // force rebuild
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals-v2.css";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
