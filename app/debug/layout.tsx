af458ff01a4f1381a071f140110c7a722dcd618b
mmanwu2026 <info@taragynecology.com>
2026-06-08 22:38:15 -0400

    delete debug folder

--- app/debug/layout.tsx
+++
@@ -1,7 +0,0 @@
-// webhook test
-// debug layout updated
-import { ReactNode } from "react";
-
-export default function DebugLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
