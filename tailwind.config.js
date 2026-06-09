47369d321db08b7f015c5af949f63d90d09482ad
mmanwu2026 <info@taragynecology.com>
2026-06-09 00:12:12 -0400

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+      cursor: ["disabled"],
+      backgroundColor: ["disabled"],
+    },
+  },
+  plugins: [],
+};
