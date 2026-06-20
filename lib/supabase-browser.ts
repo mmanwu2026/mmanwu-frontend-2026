ca4f124c63b7c88d2432c5622c6e34dd29743617
Chukwudi Nwawka <info@taragynecology.com>
2026-06-20 14:12:13 -0400

    Expose supabase-browser globally

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -3,8 +3,15 @@
 import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
 
 export function createSupabaseBrowserClient() {
-  return createBrowserClient(
+  const client = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+
+  if (typeof window !== "undefined") {
+    // @ts-ignore
+    window.supabase = client;
+  }
+
+  return client;
 }
