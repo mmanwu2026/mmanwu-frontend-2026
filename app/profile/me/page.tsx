a00a646f56a3a378c953ff1522015a402509ed6a
Chukwudi Nwawka <info@taragynecology.com>
2026-06-23 16:36:09 -0400

    Fix Profile me

--- app/profile/me/page.tsx
+++ app/profile/me/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { redirect } from "next/navigation";
 import { useUser } from "@/context/UserContext";
+
+export const dynamic = "force-dynamic";
 
 export default function ProfileMeRedirect() {
   const { user, loading } = useUser();
