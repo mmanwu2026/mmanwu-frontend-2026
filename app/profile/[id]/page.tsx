22a0ab36834e62c1c06b29ecbe9306687be8c09c
Chukwudi Nwawka <info@taragynecology.com>
2026-06-24 16:10:09 -0400

    Update Profile page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,5 +1,7 @@
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export const revalidate = 0;
+export const runtime = "edge";
 
 import ProfileClient from "@/components/ProfileClient";
 
