908ca3fb1adbceff1b8ee651431e43b132650a3f
Chukwudi Nwawka <info@taragynecology.com>
2026-06-24 13:46:29 -0400

    Put a wrapper to Profile Page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -3,5 +3,9 @@
 import ProfileClient from "@/components/ProfileClient";
 
 export default function Page({ params }: { params: { id: string } }) {
-  return <ProfileClient userId={params.id} />;
+  return (
+    <div className="w-full min-h-screen">
+      <ProfileClient userId={params.id} />
+    </div>
+  );
 }
