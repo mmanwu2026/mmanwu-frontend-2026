036db4748115cc0848c6227a0648bca663ff103e
Chukwudi Nwawka <info@taragynecology.com>
2026-06-24 00:31:54 -0400

    Fix Profile client and user profile

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,7 +1,7 @@
 // rebuild 003
 "use client";
 
-import ProfileClient from "./ProfileClient";
+import ProfileClient from "../ProfileClient";
 
 export default function Page({ params }: { params: { userId: string } }) {
   return <ProfileClient userId={params.userId} />;
