5f8fc183185ef04e2099ba7d76f71a21027a1369
Chukwudi Nwawka <info@taragynecology.com>
2026-06-24 15:42:33 -0400

    Switch back to Memoized Client version

--- context/SupabaseContext.tsx
+++ context/SupabaseContext.tsx
@@ -11,7 +11,7 @@ export function SupabaseProvider({ children }: { children: React.ReactNode }) {
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
-  }, []);
+  }, []); // ⭐ DO NOT REMOVE THIS
 
   return (
     <SupabaseContext.Provider value={supabase}>
