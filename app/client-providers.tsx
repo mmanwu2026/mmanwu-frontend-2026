bb5db4892b6b7cf333bd77e5113bdf9a0f939e5b
Chukwudi Nwawka <info@taragynecology.com>
2026-07-06 01:37:14 -0400

    comment out Unread Provider

--- app/client-providers.tsx
+++ app/client-providers.tsx
@@ -2,15 +2,15 @@
 
 import { SupabaseProvider } from "@/context/SupabaseContext";
 import { UserProvider } from "@/context/UserContext";
-import { UnreadProvider } from "@/context/UnreadContext";
+
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <SupabaseProvider>
       <UserProvider>
-        <UnreadProvider>
+        
           {children}
-        </UnreadProvider>
+       
       </UserProvider>
     </SupabaseProvider>
   );
