fb81d05d21149bd9d73992da3eba591ed1d95b71
Chukwudi Nwawka <info@taragynecology.com>
2026-07-07 14:47:13 -0400

    update SW

--- app/sw-register.tsx
+++ app/sw-register.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function SWRegister() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").then(() => {
+      navigator.serviceWorker.register("/sw-v2.js").then(() => {
         console.log("Service worker registered");
       });
     }
