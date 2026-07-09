e7847545096ce45b6738656fe94e0f0f5d6fc9c1
Chukwudi Nwawka <info@taragynecology.com>
2026-07-09 14:20:48 -0400

    Update sendPush

--- lib/sendPush.ts
+++ lib/sendPush.ts
@@ -1,10 +1,17 @@
-export async function sendPush(subscription: any, payload: any) {
-  await fetch(
-    "https://dnhklmhwbkfhbolskqnt.supabase.co/functions/v1/send-push",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ subscription, payload }),
-    }
-  );
+export async function sendPush(targetUserId: string, payload: any) {
+  try {
+    await fetch(
+      "https://dnhklmhwbkfhbolskqnt.supabase.co/functions/v1/send-push",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          targetUserId,
+          payload,
+        }),
+      }
+    );
+  } catch (err) {
+    console.error("sendPush error:", err);
+  }
 }
