253749226ab38cf09ef319394a48ee3e0954677b
Chukwudi Nwawka <info@taragynecology.com>
2026-07-12 18:41:03 -0400

    fix providers wrapper for sticky AuthNav

--- app/providers-wrapper.tsx
+++ app/providers-wrapper.tsx
@@ -9,16 +9,15 @@ import UpdateBanner from "@/components/UpdateBanner";
 export default function ProvidersWrapper({ children }: { children: React.ReactNode }) {
   return (
     <ClientProviders>
-      {/* Global banner */}
       <UpdateBanner />
 
-      {/* AuthNav now handles navigation to /composer */}
-      <AuthNav />
-
+      {/* ⭐ AuthNav stays inside providers (Supabase + Unread), 
+          but ABOVE scrollable page content */}
       <UnreadProvider>
+        <AuthNav />
         <SWRegister />
 
-        {/* Page content */}
+        {/* ⭐ Page content (scrolls BELOW AuthNav) */}
         {children}
       </UnreadProvider>
     </ClientProviders>
