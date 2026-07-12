bc77a95ece268650ddb0b8c59c16369b483cd12a
Chukwudi Nwawka <info@taragynecology.com>
2026-07-12 17:44:22 -0400

    Sticky AuthNav

--- components/AuthNav.tsx
+++ components/AuthNav.tsx
@@ -49,7 +49,7 @@ export default function MobileAuthNav() {
   }
 
   return (
-    <div className="relative w-full bg-white border-b border-gray-200 px-4 py-2 flex items-center">
+    <div className="sticky top-0 z-[5000] relative w-full bg-white border-b border-gray-200 px-4 py-2 flex items-center">
 
       {/* ⭐ LEFT SIDE ICONS */}
       <div className="flex items-center gap-4 text-gray-700">
