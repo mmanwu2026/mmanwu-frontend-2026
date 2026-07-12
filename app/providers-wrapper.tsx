4c486469660ffeaafb88eb0b857201acbeb48663
Chukwudi Nwawka <info@taragynecology.com>
2026-07-12 13:37:06 -0400

    fix providers-wrapper

--- app/providers-wrapper.tsx
+++ app/providers-wrapper.tsx
@@ -9,12 +9,21 @@ import UpdateBanner from "@/components/UpdateBanner";
 export default function ProvidersWrapper({ children }: { children: React.ReactNode }) {
   return (
     <ClientProviders>
-      <UpdateBanner />
-      <AuthNav />
-      <UnreadProvider>
-        <SWRegister />
-        {children}
-      </UnreadProvider>
+      {/* ⭐ FIX: Give ProvidersWrapper a full-height flex column */}
+      <div className="h-full flex flex-col">
+        <UpdateBanner />
+        <AuthNav />
+
+        {/* ⭐ FIX: UnreadProvider must NOT collapse height */}
+        <UnreadProvider>
+          <SWRegister />
+
+          {/* ⭐ children (PlazaPage) now inherits full height correctly */}
+          <div className="flex-1 flex flex-col h-full">
+            {children}
+          </div>
+        </UnreadProvider>
+      </div>
     </ClientProviders>
   );
 }
