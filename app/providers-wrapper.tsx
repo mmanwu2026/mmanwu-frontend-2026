31c609fa781348c6871f9ecddde501fc213231b2
Chukwudi Nwawka <info@taragynecology.com>
2026-07-13 15:03:42 -0400

    Debug BottomNav

--- app/providers-wrapper.tsx
+++ app/providers-wrapper.tsx
@@ -7,28 +7,26 @@ import SWRegister from "./sw-register";
 import UpdateBanner from "@/components/UpdateBanner";
 import BottomNav from "@/app/components/BottomNav";
 
-export default function ProvidersWrapper({ children }) {
+export default function ProvidersWrapper({ children }: { children: React.ReactNode }) {
   return (
     <ClientProviders>
       <UnreadProvider>
 
-        {/* Top navigation */}
-        <div className="fixed top-0 left-0 w-full z-[2147483647] pointer-events-auto">
+        {/* Top nav */}
+        <div className="fixed top-0 left-0 w-full z-[1000]">
           <AuthNav />
         </div>
 
         <UpdateBanner />
         <SWRegister />
 
-        {/* ⭐ Add bottom padding so feed does NOT cover BottomNav */}
-        <div className="pt-[60px] pb-[70px] w-full relative z-0">
+        {/* Page content */}
+        <div className="pt-[60px] pb-[80px] w-full">
           {children}
         </div>
 
-        {/* Bottom navigation */}
-        <div className="fixed bottom-0 left-0 w-full z-[2147483647] pointer-events-auto">
-          <BottomNav />
-        </div>
+        {/* Bottom nav */}
+        <BottomNav />
 
       </UnreadProvider>
     </ClientProviders>
